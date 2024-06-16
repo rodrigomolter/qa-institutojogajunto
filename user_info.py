56ffec2830840b502eacdb283e6289685572a165
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-06-16 20:34:56 -0300

    :recycle: refactor: deixando no try catch apenas as conversões

--- user_info.py
+++ user_info.py
@@ -12,13 +12,12 @@ try:
   idade = int(input("Qual a sua idade? "))
   nota_um = int(input("Qual nota você tirou no PRIMEIRO teste? "))
   nota_dois = int(input("E por último, qual nota você tirou no SEGUNDO teste? "))
-  total = nota_um + nota_dois
-  media = total/2
 except ValueError:
   print("Por favor, utilize apenas números!")
   sys.exit()
 
-
+total = nota_um + nota_dois
+media = total/2
 
 print(f"\nBoa noite, {nome.capitalize()}! Você possui {idade} anos e sua altura é de {altura}m.")
 print(f"Suas notas sommadas dão um total de {total} pontos e uma media de {media}")
