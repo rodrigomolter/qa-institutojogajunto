6993f190fa0cf574202d0da1ee520acb1db934b4
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-06-16 20:29:30 -0300

    :sparkles: feat: funcionalidade de notas no prog de dados do usuario

--- user_info.py
+++ user_info.py
@@ -7,20 +7,18 @@ Faça um programa que capture o nome do usuário, altura em metros, idade e impr
 print("Bem vindo ao programa do Squad 4 da Comunidade !\n")
 
 nome = input("Qual o seu nome? ")
-
-altura = input("Qual a sua altura? (em metros) ")
 try:
-  altura = float(altura)
+  altura = float(input("Qual a sua altura? (em metros) "))
+  idade = int(input("Qual a sua idade? "))
+  nota_um = int(input("Qual nota você tirou no PRIMEIRO teste? "))
+  nota_dois = int(input("E por último, qual nota você tirou no SEGUNDO teste? "))
+  total = nota_um + nota_dois
+  media = total/2
 except ValueError:
-  print("Por favor, insira a altura em metros!")
-  sys.exit()
-
-idade = input("E por último, qual a sua idade? ")
-try:
-  idade = int(idade)
-except ValueError:
-  print("Por favor, informe a idade utilizando apenas números")
+  print("Por favor, utilize apenas números!")
   sys.exit()
 
 
-print(f" Boa noite, {nome.capitalize()}! Você possui {idade} anos e sua altura é de {altura}m.")
+
+print(f"\nBoa noite, {nome.capitalize()}! Você possui {idade} anos e sua altura é de {altura}m.")
+print(f"Suas notas sommadas dão um total de {total} pontos e uma media de {media}")
