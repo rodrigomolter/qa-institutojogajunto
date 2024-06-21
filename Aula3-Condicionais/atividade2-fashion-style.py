dbe601b4516fc11edf087b11356b4a25bd377cc8
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-06-20 22:03:16 -0300

    :coffin: refactor: removido código não utilizado

--- Aula3-Condicionais/atividade2-fashion-style.py
+++ Aula3-Condicionais/atividade2-fashion-style.py
@@ -11,21 +11,6 @@ Caso o cliente faça uma compra acima de R$250,00 "PARABÉNS. VOCÊ GANHOU 10% D
 Caso o cliente faça uma compra acima de R$500,00 "PARABÉNS. VOCÊ GANHOU SUPER DESCONTO DE 30%"
 """
 
-# print("Bem vindo a FashionStyle")
-# valor = int(input("Qual o valor da compra? "))
-# desconto = 0
-# if valor >= 500:
-#   print("PARABÉNS. VOCÊ GANHOU SUPER DESCONTO DE 30%")
-#   desconto = 0.3
-#   print()
-# elif valor >= 250:
-#   desconto = 0.1
-#   print("PARABÉNS. VOCÊ GANHOU 10% DE DESCONTO, MAS PODE GANHAR 30% SE SUA COMPRA FOR ACIMA DE R$500,00")
-#   print(f"FALTA {500-valor} para ganhar 30% de desconto")
-# else:
-#   print("POXA, FALTA POUCO PARA VOCÊ GANHAR 10% DE DESCONTO EM SUA COMPRA.")
-#   print(f"Adquire mais {250-valor}")
-
 valor_compra = float(input("Informe o valor da sua compra para verificar elegibilidade para desconto: "))
 
 if valor_compra > 500.00:
