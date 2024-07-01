6fb977f565e3f5912c22d0f1f2ae8d54c5afba09
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-07-01 19:30:10 -0300

    :recycle: ref: usando snake case

--- Aula6-Funcoes/divisao_alunos.py
+++ Aula6-Funcoes/divisao_alunos.py
@@ -1,10 +1,10 @@
-def isPar(numero: int) -> bool:
+def is_par(numero):
   return numero % 2 == 0
 
 print("BEM VINDO A OLIMPIADA DO CONHECIMENTO")
 matricula = int(input("Digite a sua matricula: "))
 
-if isPar(matricula):
+if is_par(matricula):
   print("VOCÊ ESTÁ NO TIME AZUL")
 else:
   print("VOCÊ ESTÁ NO TIME AMARELO")
