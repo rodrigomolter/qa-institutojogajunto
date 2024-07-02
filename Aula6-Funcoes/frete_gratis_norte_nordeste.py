e499b59017730365aec85ebaec945ddfa8a72605
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-07-01 22:05:29 -0300

    :recycle: refactor: adicionando mais type hints

--- Aula6-Funcoes/frete_gratis_norte_nordeste.py
+++ Aula6-Funcoes/frete_gratis_norte_nordeste.py
@@ -4,29 +4,29 @@ ENDPOINT = "https://viacep.com.br/ws"
 FORMATO_RETORNO_API = "json"
 
 def main():
-  cep = input("Informe seu CEP: ")
+  cep: str = input("Informe seu CEP: ")
   if is_cep_valid(cep):
     if tem_frete_gratis(cep):
       print("Parabéns, você ganhou frete grátis!")
     else:
       print(f"O valor do frete para {cep} é R$19,90")
   else:
-    print("CEP INVALIDO")
+    print(f"CEP {cep} NÃO É UM CEP VALIDO")
 
 def tem_frete_gratis(cep: str) -> bool:
-  dados = consulta_cep(cep)
+  dados: dict = consulta_cep(cep)
   return is_norte_nordeste(dados['uf'])
 
 def consulta_cep(cep: str) -> dict:
-  response = requests.get(f"{ENDPOINT}/{cep}/{FORMATO_RETORNO_API}")
+  response: requests.Response = requests.get(f"{ENDPOINT}/{cep}/{FORMATO_RETORNO_API}")
   return response.json()
 
 def is_norte_nordeste(uf: str) -> bool:
   UF_NORTE_NORDESTE = ("AC", "AP", "AM", "PA", "RO", "RR", "TO", "AL", "BA", "CE", "MA", "PB", "PE", "PI", "RN", "SE")
   return uf.upper() in UF_NORTE_NORDESTE
 
-def is_cep_valid(cep):
-  return len(cep) == 8
+def is_cep_valid(cep: str) -> bool:
+  return len(cep) == 8 and cep.isdigit()
 
 if __name__ == "__main__":
   main()
