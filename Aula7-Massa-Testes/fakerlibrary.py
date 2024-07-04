bdafc2a07f48713232b93fe8de8cb7c52d4e6a4c
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-07-04 20:08:57 -0300

    :sparkles: feat: adicionando várias personas; salvando como csv

--- Aula7-Massa-Testes/fakerlibrary.py
+++ Aula7-Massa-Testes/fakerlibrary.py
@@ -3,19 +3,28 @@ Uma função para criar personas, contendo nome, cidade, idade.
 Salve os dados dessas personas em um arquivo CSV.
 Suba todos os arquivos para seu repositório.
 """
-
 from faker import Faker
-import json
+import pandas as pd
+import os
 
-faker = Faker('pt_BR')
+def main() -> None:
+  df = pd.DataFrame(create_personas(3))
+  save_to_csv('personas.csv', df)
+  
+def create_persona() -> dict:
+  fake = Faker('pt_BR')
+  data = {
+    "nome": fake.name(),
+    "cidade": fake.city(),
+    "idade": fake.random_int(18, 100)
+  }
+  return data
 
-persona = {
-  "nome" : faker.name(),
-  "idade" : faker.random_int(min=18, max=99),
-  "cidade" : faker.city()
-}
+def create_personas(number_of_personas: int) -> list:
+    return [create_persona() for _ in range(number_of_personas)]
 
-print(persona)
+def save_to_csv(file_name: str, dataframe: pd.DataFrame) -> None:
+  file_exists = os.path.isfile(file_name)
+  dataframe.to_csv(file_name, index=False, mode='a', header=not file_exists)
 
-with open('persona.csv', 'w') as file:
-  json.dump(persona, file)
+main()
