4e1abca9fdfb0b796585ba73099d42f9e8b4d35a
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-07-15 20:30:13 -0300

    :recycle: refactor: encapsulando funcoes para reaproveitamento

--- Aula8-Automacao/formulario_ijj.py
+++ Aula8-Automacao/formulario_ijj.py
@@ -3,22 +3,51 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from faker import Faker
+
+BTN_REJECT_COOKIES = (By.ID, "adopt-reject-all-button")
+SUBMIT_FORM_BUTTON = (By.XPATH, "//button[@type='submit']")
+NAME_INPUT = (By.ID, "nome")
+EMAIL_INPUT = (By.ID, "email")
+SUBJECT_INPUT = (By.ID, "assunto")
+MESSAGE_INPUT = (By.ID, "mensagem")
 
 browser = Firefox()
+def main() -> None:
+  browser.get("https://www.jogajuntoinstituto.org/")
+  reject_all_cookes()
+  # fill_contact_form(
+  #   nome="Rodrigo Molter", 
+  #   email="rodrigo.molter@gmail.com", 
+  #   mensagem="Olá, humano! BipBop 🤖\n\n Esta mensagem esta sendo enviada de forma automatizada com Selenium Webdriver e Python.", 
+  #   assunto="Ser facilitador"
+  #   )
+  fill_contact_form()
+  submit_contact_form()
 
-browser.get("https://www.jogajuntoinstituto.org/")
+  WebDriverWait(browser, 15).until(EC.alert_is_present())
+  assert browser.switch_to.alert.text == "Dados recebidos!"
 
-WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.ID, "adopt-reject-all-button")))
-browser.find_element(By.ID, "adopt-reject-all-button").click()
+  browser.quit()
 
-browser.find_element(By.ID, "nome").send_keys("Rodrigo Molter")
-browser.find_element(By.ID, "email").send_keys("rodrigo.molter@gmail.com")
-browser.find_element(By.ID, "mensagem").send_keys("Olá, humano! BipBop 🤖\n\n Esta mensagem esta sendo enviada de forma automatizada com Selenium Webdriver e Python.")
-Select(browser.find_element(By.ID, "assunto")).select_by_value("Ser facilitador")
+def reject_all_cookes() -> None:
+  WebDriverWait(browser, 5).until(EC.presence_of_element_located(BTN_REJECT_COOKIES))
+  browser.find_element(*BTN_REJECT_COOKIES).click()
 
-browser.find_element(By.XPATH, "//button[@type='submit']").click()
+def fill_contact_form(nome: str = None, email: str = None, assunto: str = None, mensagem: str = None ) -> None:
+  fake = Faker()
 
-WebDriverWait(browser, 15).until(EC.alert_is_present())
-assert browser.switch_to.alert.text == "Dados recebidos!"
+  nome = nome or fake.name()
+  email = email or fake.email()
+  assunto = assunto or "Ser facilitador"
+  mensagem = mensagem or fake.paragraph()
 
-browser.quit()
+  browser.find_element(*NAME_INPUT).send_keys(nome)
+  browser.find_element(*EMAIL_INPUT).send_keys(email)
+  browser.find_element(*MESSAGE_INPUT).send_keys(mensagem)
+  Select(browser.find_element(*SUBJECT_INPUT)).select_by_value(assunto)
+
+def submit_contact_form() -> None:
+  browser.find_element(*SUBMIT_FORM_BUTTON).click()
+
+main()
--- Aula8-Automacao/google_search.py
+++ Aula8-Automacao/google_search.py
@@ -4,18 +4,27 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+SEARCH_INPUT = (By.NAME, "q")
+SEARCH_RESULTS = (By.CSS_SELECTOR, "#search a h3")
+
 browser = Firefox()
-browser.get("https://www.google.com")
 
-browser.find_element(By.NAME, "q").send_keys(f"Instituto Joga Junto {Keys.ENTER}")
+def main() -> None:
+  browser.get("https://www.google.com")
 
-WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#search a h3")))
-results = browser.find_elements(By.CSS_SELECTOR, "#search a h3")
+  results = google_search_for("Instituto Joga Junto")
 
-for result in results:
-  if result.text == "Instituto Joga Junto":
-    result.click()
-    break
+  for result in results:
+    if result.text == "Instituto Joga Junto":
+      result.click()
+      break
 
-assert browser.title == "Instituto Joga Junto", "Título da página não era Instituto Joga Junto"
-browser.quit()
+  assert browser.title == "Instituto Joga Junto", "Título da página não era Instituto Joga Junto"
+  browser.quit()
+
+def google_search_for(query: str) -> None:
+  browser.find_element(*SEARCH_INPUT).send_keys(f"{query}{Keys.ENTER}")
+  WebDriverWait(browser, 5).until(EC.presence_of_element_located(SEARCH_RESULTS))
+  return browser.find_elements(*SEARCH_RESULTS)
+
+main()
